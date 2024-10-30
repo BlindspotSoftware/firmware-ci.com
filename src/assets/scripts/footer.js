d8aa914e695d1ae91f45a27ea5e0f3d87d8827b9
nd55 <45429767+nd55@users.noreply.github.com>
2024-10-30 16:09:37 +0100

    add footer

---
+++ src/assets/scripts/footer.js
@@ -0,0 +1,16 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".footer__shape path", {
+  yPercent: 50,
+  stagger: 0.1,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".footer__shape",
+    start: "top bottom",
+    end: "bottom bottom",
+    scrub: 1,
+  },
+});
