667f7938495adbe66ea5981ae99f666a6655eafe
nd55 <45429767+nd55@users.noreply.github.com>
2024-11-14 14:07:44 +0100

    improve mobile nav behaviour

--- src/assets/scripts/nav.js
+++ src/assets/scripts/nav.js
@@ -1,8 +1,17 @@
 const navToggle = document.getElementById("nav-toggle");
+const headerLinks = document.querySelectorAll(".header a");
 
+// Mobile nav toggle
 navToggle.addEventListener("click", () => {
   navToggle.setAttribute(
     "aria-expanded",
     navToggle.getAttribute("aria-expanded") === "true" ? "false" : "true"
   );
+});
+
+// Close the nav when a link is clicked
+headerLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navToggle.setAttribute("aria-expanded", "false");
+  });
 });
